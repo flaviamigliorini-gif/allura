ba38beb5c8ee15b35a82ddae1e7e3be40096d5d2
flaviamigliorini-gif <flavia.migliorini@escola.pr.gov.br>
2026-07-30 07:50:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-// Aguarda que a página seja completamente carregada antes de atribuir os eventos
 document.addEventListener('DOMContentLoaded', () => {
     const actionBtn = document.getElementById('actionBtn');
 
     if (actionBtn) {
         actionBtn.addEventListener('click', () => {
-            alert('Para apoiar, busque por ONGs internacionais reconhecidas como a UN Women, Amnesty International e Fundo Malala que atuam na defesa dos direitos educacionais das mulheres afegãs.');
+            alert('Para apoiar a causa, busque por ONGs internacionais reconhecidas, como UN Women, Amnesty International e Fundo Malala, que atuam na defesa dos direitos e da educação das mulheres afegãs.');
         });
     }
 });
